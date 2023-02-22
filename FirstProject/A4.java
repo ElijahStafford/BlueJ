1d6bbd7ef91cfb79efe1507a3738093f95c29bfb
ElijahStafford <124601746+ElijahStafford@users.noreply.github.com>
2023-02-22 10:08:22 -0500

    finish a4

--- FirstProject/A4.java
+++ FirstProject/A4.java
@@ -6,10 +6,10 @@ public class A4 {
 
         var randIndex = (int)Math.round(Utils.Random(0, myString.length() - 1)); 
         var randString = myString.substring(0, randIndex);
-        randString += myString.substring(randIndex + 1, myString.length());
+        randString += myString.substring(randIndex + 1);
 
-        // .compareTo return the difference of lengths from one string to another
-        // .equals is a boolean which tells whether 2 strings have the same characters
+        // .compareTo() returns the difference of lengths from one string to another
+        // .equals() is a boolean which tells whether 2 strings have the same characters
 
         Utils.Log("Original: " + myString +
         "\nString length: " + myString.length() +
@@ -19,17 +19,32 @@ public class A4 {
         "\nIndex of i: " + myString.indexOf("i") +
         "\nIndex of # (none): " + myString.indexOf("#") +
         "\nFirst 3 characters: " + myString.substring(0, 3) +
-        "\nThird to 11th: " + myString.substring(2, 11) + 
+        "\nThird to 10th: " + myString.substring(2, 10) + 
         "\nRandom character removed: " + randString);
 
         var customString = "abcdefghijklmnopqrstuv";
         var charArr = customString.toCharArray();
 
-        Utils.Log("Original: " + customString +
+        Utils.Log("\nOriginal: " + customString +
         "\nString length: " + customString.length() + 
-        "\nLess than: \"aaaa\" = " + customString.compareTo("aaaa") +
-        "\nGreater than: \"abcdefghijklmnopqrstuvqrstuv\" = " + customString.compareTo("abcdefghijklmnopqrstuvqrstuv") +
+        "\nLess than: \"abcdefghijklmnop\" = " + "abcdefghijklmnop".compareTo(customString) +
+        "\nGreater than: \"abcdefghijklmnopqrstuvqrstuv\" = " + "abcdefghijklmnopqrstuvqrstuv".compareTo(customString) +
         "\nEqual to: \"ABCDEFGHIJKLMNOPQRSTUV\" = " + customString.compareTo("ABCDEFGHIJKLMNOPQRSTUV") +
-        "\nLetters 4 and 17: " + charArr[3] + " " + charArr[16]);
+        "\nLetters 4 and 17: " + charArr[3] + ", " + charArr[16]);
+        
+        Utils.Log("Characters in order:");
+        for (var i = 0; i < charArr.length; i++) {
+            Utils.Log(charArr[i]);
+        }
+        
+        Utils.Log("Characters backward:");
+        for (var i = charArr.length - 1; i >= 0; i--) {
+            Utils.Log(charArr[i]);
+        }
+        
+        Utils.Log("Every second character:");
+        for (var i = 0; i < charArr.length; i += 2) {
+            Utils.Log(charArr[i]);
+        }
     }
 }
